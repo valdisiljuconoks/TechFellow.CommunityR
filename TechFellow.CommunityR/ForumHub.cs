db53eb62bd487a4e590551ce7c9429dc9c497ffd
Valdis Iljuconoks <valdis@tech-fellow.lv>
2013-07-01 23:10:41 +0300

    Initial version, added OnTopicAdded event

---
+++ TechFellow.CommunityR/ForumHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace TechFellow.CommunityR
+{
+    public class ForumHub : Hub
+    {
+        internal const string CommunityClubGroupName = "community-club";
+
+        public Task JoinClub(string id)
+        {
+            return Groups.Add(Context.ConnectionId, CommunityClubGroupName + id);
+        }
+
+        public Task LeaveClub(string id)
+        {
+            return Groups.Remove(Context.ConnectionId, CommunityClubGroupName + id);
+        }
+    }
+}
---
+++ TechFellow.CommunityR/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TechFellow.CommunityR")]
+[assembly: AssemblyDescription("EPiServer Relate+ server side events pushed to client side via SignalR")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommunityR")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fc33bf9b-d9dc-48a9-88a1-d03503ef5cb8")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1")]
